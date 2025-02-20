201c76842f29ca9306976b180800d3f6be88e860
Ram Prakash Pathak <133332560+RamP1432@users.noreply.github.com>
2025-02-20 11:09:39 +0530

    [Ram Prakash Pathak] Merge two JSON objects into one.

---
+++ main/java/com/tit/json/practiceproblems/Problem4.java
@@ -0,0 +1,21 @@
+package com.tit.json.practiceproblems;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+import java.io.File;
+import java.io.IOException;
+
+public class Problem4{
+    public static void main(String[] args) throws IOException {
+      ObjectMapper objectMapper=new ObjectMapper();
+        JsonNode node1=objectMapper.readTree(new File("C:\\Users\\patha\\OneDrive\\Desktop\\Week5_Day2\\src\\main\\resources\\JSON4.0.json"));
+        JsonNode node2=objectMapper.readTree(new File("C:\\Users\\patha\\OneDrive\\Desktop\\Week5_Day2\\src\\main\\resources\\JSON4.1.json"));
+            ObjectNode mergedNode = (ObjectNode) node1;
+            mergedNode.setAll((ObjectNode) node2);
+            // Print or save the merged JSON
+        System.out.println(objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(mergedNode));
+        objectMapper.writerWithDefaultPrettyPrinter().writeValue(new File("C:\\Users\\patha\\OneDrive\\Desktop\\Week5_Day2\\src\\main\\resources\\JSON4merged.json"),mergedNode);
+
+    }
+}
